4ec9220818285265ba9ef57b7bd6d7c921e3fe6d
ragaamaherhanna2 <ragaamaherhanna@gmail.com>
2017-07-31 08:13:54 +0200

    Adding the  database and favorite recipe to the widget

--- app/src/main/java/com/example/marian/finalbakingapp/activity/StepListActivity.java
+++ app/src/main/java/com/example/marian/finalbakingapp/activity/StepListActivity.java
@@ -1,22 +1,34 @@
 package com.example.marian.finalbakingapp.activity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.marian.finalbakingapp.R;
 import com.example.marian.finalbakingapp.adapter.StepAdapter;
 import com.example.marian.finalbakingapp.fragment.stepDetailFragment;
+import com.example.marian.finalbakingapp.model.Ingredient;
 import com.example.marian.finalbakingapp.model.Recipe;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import static com.example.marian.finalbakingapp.activity.MainActivity.RECIPE;
-
+import static com.example.marian.finalbakingapp.database.Contract.RECIPE_CONTENT_URI;
+import static com.example.marian.finalbakingapp.database.Contract.RecipeEntry;
 /**
  * An activity representing a list of Items. This activity
  * has different presentations for handset and tablet-size devices. On
@@ -80,7 +92,8 @@ public class StepListActivity extends AppCompatActivity implements StepAdapter.O
 
 
     @Override
-    public void onRecipeSelected(int position) {
+    public void onRecipeSelected(int position)
+    {
         Bundle arguments = new Bundle();
 
         if (mTwoPane) {
@@ -113,6 +126,90 @@ public class StepListActivity extends AppCompatActivity implements StepAdapter.O
         }
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_favorite_w, menu);
+        return true;
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_widget:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    if (isFavorite()) {
+                        deleteFromWidget();
+                        item.setIcon(R.drawable.ic_favorite_border_black_24dp);
+                        Toast.makeText(this, String.format(getString(R.string.remove_from_widget), recipe.getName()), Toast.LENGTH_LONG).show();
+                    } else {
+                        addToWidget();
+                        item.setIcon(R.drawable.ic_favorite_black_24dp);
+                        Toast.makeText(this, String.format(getString(R.string.add_to_widget), recipe.getName()), Toast.LENGTH_LONG).show();
+                    }
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.findItem(R.id.action_widget);
+        if (isFavorite()) {
+            menuItem.setIcon(R.drawable.ic_favorite_black_24dp);
+        }
+        else {
+            menuItem.setIcon(R.drawable.ic_favorite_border_black_24dp);
+        }
+        return true;
+    }
+
+
+    synchronized private void deleteFromWidget() {
+        getContentResolver().delete(RECIPE_CONTENT_URI, null, null);
+    }
+
+    synchronized private void addToWidget() {
+        getContentResolver().delete(RECIPE_CONTENT_URI, null, null);
+        getDetails(recipe.getIngredients());
+    }
+
+
+    private void getDetails(ArrayList<Ingredient> ingredients) {
+
+        for (Ingredient ingredient : ingredients) {
+            ContentValues values = new ContentValues();
+            values.put(RecipeEntry.COLUMN_RECIPE_ID, recipe.getId());
+            values.put(RecipeEntry.COLUMN_RECIPE_NAME, recipe.getName());
+            values.put(RecipeEntry.COLUMN_INGREDIENT_NAME, ingredient.getAll());
+            values.put(RecipeEntry.COLUMN_INGREDIENT_QUANTITY, ingredient.getQuantity());
+            values.put(RecipeEntry.COLUMN_INGREDIENT_MEASURE, ingredient.getMeasure());
+            getContentResolver().insert(RECIPE_CONTENT_URI, values);
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private boolean isFavorite() {
+        String[] projection = {RecipeEntry.COLUMN_RECIPE_ID};
+        String selection = RecipeEntry.COLUMN_RECIPE_ID + " = " + recipe.getId();
+        @SuppressLint("Recycle") Cursor cursor = getContentResolver().query(RECIPE_CONTENT_URI,
+                projection,
+                selection,
+                null,
+                null,
+                null);
+
+        return (cursor != null ? cursor.getCount() : 0) > 0;
+    }
+
 }
 
 
